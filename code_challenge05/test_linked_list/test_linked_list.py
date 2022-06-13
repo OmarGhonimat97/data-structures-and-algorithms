4eab575bbf77c8badeeb5d9ddc000164b7f60963
OmarGhonimat97 <omar.alghonimat@gmail.com>
2022-06-13 20:21:00 +0300

    CodeChallenge06

--- code_challenge05/test_linked_list/test_linked_list.py
+++ code_challenge05/test_linked_list/test_linked_list.py
@@ -53,6 +53,30 @@ def test_includes_true(LL):
 def test_includes_false(LL):
     assert LL.includes(13) == False
 
+def test_append(LL2):
+    actual = LL2.head.next.next.value
+    expected = 1
+    assert actual == expected
+
+def test_insert_before(LL3):
+    actual = LL3.head.next.value
+    expected = 8
+    assert actual == expected
+
+def test_insert_before_beginning(LL5):
+    actual = LL5.head.value
+    expected = 9
+    assert actual == expected
+
+def test_insert_after(LL4):
+    actual = LL4.head.next.next.value
+    expected = 8
+    assert actual == expected
+
+def test_insert_after_end(LL6):
+    actual = LL6.head.next.next.next.value
+    expected = 9
+    assert actual == expected
 
 @pytest.fixture
 def node():
@@ -77,3 +101,53 @@ def LL():
     LL.insert(7)
     LL.includes(5)
     return LL
+
+# test append
+@pytest.fixture
+def LL2():
+    LL2 = LinkedList()
+    LL2.append(5)
+    LL2.append(3)
+    LL2.append(1)
+    return LL2
+
+# test insert before
+@pytest.fixture
+def LL3():
+    LL3 = LinkedList()
+    LL3.append(5)
+    LL3.append(3)
+    LL3.append(1)
+    LL3.insert_before(3, 8)
+    return LL3
+
+# test insert before beginning
+@pytest.fixture
+def LL5():
+    LL5 = LinkedList()
+    LL5.insert(3)
+    LL5.insert(2)
+    LL5.insert(1)
+    LL5.insert_before(1, 9)
+    return LL5
+
+
+# test insert after
+@pytest.fixture
+def LL4():
+    LL4 = LinkedList()
+    LL4.append(5)
+    LL4.append(3)
+    LL4.append(1)
+    LL4.insert_after(3, 8)
+    return LL4
+
+# test insert after ending
+@pytest.fixture
+def LL6():
+    LL6 = LinkedList()
+    LL6.insert(3)
+    LL6.insert(2)
+    LL6.insert(1)
+    LL6.insert_after(3, 9)
+    return LL6
