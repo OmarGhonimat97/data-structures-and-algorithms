37640327fcb44bab0d9f5fac47594c4aca20a4cd
OmarGhonimat97 <omar.alghonimat@gmail.com>
2022-08-29 20:26:23 +0300

    graph_depth_first_pre_order

---
+++ graph_depth_first/graph_depth_first.py
@@ -0,0 +1,140 @@
+from collections import deque
+
+
+class Queue:
+    def __init__(self):
+        self.deque = deque()
+
+    def enqueue(self, value):
+        self.deque.append(value)
+
+    def dequeue(self):
+        return self.deque.popleft()
+
+    def __len__(self):
+        return len(self.deque)
+
+
+class Vertex:
+    def __init__(self, value):
+        """
+        Node/vertex constructor
+        """
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class Edge:
+    def __init__(self, vertex, weight=0):
+        self.weight = weight
+        self.vertex = vertex
+
+
+class Graph:
+    def __init__(self):
+        self.__adjacency_list = {}
+
+    def add_node(self, value):
+        """ Add the node to adj_list as key and value it will be an empty list"""
+        vertex = Vertex(value)
+        self.__adjacency_list[vertex] = []
+        return vertex
+
+    def add_edge(self, start_vertex, end_vertex, weight=0):
+        """
+        Adds a new edge between two nodes in the graph
+        2 nodes to be connected by the edge, weight
+        """
+        if start_vertex not in self.__adjacency_list or end_vertex not in self.__adjacency_list:
+            raise KeyError("Start or End Vertex not found")
+
+        if len(self.__adjacency_list) == 1: # For the case of having a graph with one vertex and one edge
+            edge = Edge(end_vertex, weight)
+            self.__adjacency_list[start_vertex].append(edge)
+        else:
+            edge1 = Edge(end_vertex, weight)
+            edge2 = Edge(start_vertex, weight)
+            self.__adjacency_list[start_vertex].append(edge1)
+            self.__adjacency_list[end_vertex].append(edge2)
+
+    def get_nodes(self):
+        """
+        Returns all the nodes in the graph as a collection
+        """
+        return list(self.__adjacency_list.keys())
+
+    def size(self):
+        """
+        Returns the total number of nodes in the graph
+        """
+        return len(self.__adjacency_list)
+
+    def get_neighbors(self, vertex):
+        """
+        Returns a collection of edges connected to the given node
+        """
+        return self.__adjacency_list.get(vertex, [])
+
+    def breadth_first(self, start_vertex):
+        """
+        Return: A collection of nodes in the order they were visited
+        """
+        result = []
+        visited = set()
+        q = Queue()
+
+        q.enqueue(start_vertex)
+        visited.add(start_vertex)
+
+        while len(q):
+            current_vertex = q.dequeue()
+            result.append(current_vertex.value)
+
+            neighbors = self.get_neighbors(current_vertex)
+
+            for edge in neighbors:
+                neighbor = edge.vertex
+                if neighbor not in visited:
+                    q.enqueue(neighbor)
+                    visited.add(neighbor)
+        return result
+
+    def depth_first(self, start_vertex):
+        """
+        Return: A collection of nodes in their pre-order depth-first traversal order
+        """
+        if start_vertex not in self.get_nodes():
+            return []
+        result = []
+
+        def traverse(vertex, visited):
+            visited.append(vertex)
+            edges = self.get_neighbors(vertex)
+            neighbors = [e.vertex for e in edges]
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    traverse(neighbor, visited)
+        traverse(start_vertex, result)
+        result_value = [i.value for i in result]
+        return result_value
+
+
+if __name__ == "__main__":
+    g = Graph()
+    a = g.add_node('A')
+    b = g.add_node('B')
+    e = g.add_node('E')
+    c = g.add_node('C')
+    d = g.add_node('D')
+    g.add_edge(a, b)
+    g.add_edge(a, e)
+    g.add_edge(a, c)
+    g.add_edge(b, d)
+    g.add_edge(b, e)
+    g.add_edge(e, d)
+    g.add_edge(e, c)
+
+    print(g.breadth_first(a))
+    print(g.depth_first(a))
